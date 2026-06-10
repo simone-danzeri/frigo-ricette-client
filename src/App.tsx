ba14e28910d33ee35d35dd165191bbe59274ae8b
simone-danzeri <s.danzeri@icloud.com>
2026-06-10 12:19:30 +0200

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom"
 function App() {
   return (
     <div>
       <h1>Frigo Ricette</h1>
+      <nav>
+        <div>
+          <Link to="/">Ingredienti</Link>
+        </div>
+        <Link to="/recipes">Ricette</Link>
+        <Link to="/groceries">Lista della spesa</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Pagina Ingredienti</div>}/>
+        <Route path="/recipes" element={<div>Pagina Ricette</div>}/>
+        <Route path="/groceries" element={<div>Lista della spesa</div>}/>
+      </Routes>
     </div>
   )
 }
